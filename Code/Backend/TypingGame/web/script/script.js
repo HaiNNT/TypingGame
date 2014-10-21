009d69dc7337837cc6d5278e39fc23fd30ae7839
Nguyễn Ngọc Thanh Hải <HaiNNT@users.noreply.github.com>
2014-10-21 14:17:34 +0700

    add, js, css, image, font

---
+++ Code/Backend/TypingGame/web/script/script.js
@@ -0,0 +1,74 @@
+/**
+ * Created by HaiNNT on 10/19/2014.
+ */
+
+/*
+Show invited matches in notification at home page
+ */
+function showInvitedMatch(){
+    var list = $("#invited-match");
+    if(list.hasClass("hidden")){
+        list.removeClass("hidden");
+    }else{
+        list.addClass("hidden");
+    }
+    var icon = $("#user-notification");
+    if(icon.hasClass("user-notification-click")){
+        icon.removeClass("user-notification-click");
+    }else{
+        icon.addClass("user-notification-click");
+    }
+}
+
+/*
+ Show profile menu at home page
+ */
+function showProfileMenu(){
+    var list = $("#profile-menu");
+    if(list.hasClass("hidden")){
+        list.removeClass("hidden");
+    }else{
+        list.addClass("hidden");
+    }
+    var icon = $("#user-menu-i");
+    if(icon.hasClass("user-notification-click")){
+        icon.removeClass("user-notification-click");
+    }else{
+        icon.addClass("user-notification-click");
+    }
+}
+
+/*
+Switch between register mode and login mode at home page
+ */
+function chooseRegister(){
+    var register = $("#register");
+    if(register.prop('checked') == true){
+        $("#authen-section .panel-header h3").text("Register");
+        $("#button-group input").val("Register");
+        $("#register-param").removeClass("hidden");
+        $("#action-type").val("register");
+    }else{
+        $("#authen-section .panel-header h3").text("Login");
+        $("#button-group input").val("Login");
+        $("#register-param").addClass("hidden");
+        $("#action-type").val("login");
+    }
+}
+
+/*
+Enable edit profile at profile page
+ */
+function editProfile(){
+    $("#profile-info").addClass("hidden");
+    $("#profile-form").removeClass("hidden");
+}
+
+/*
+Cancel edit profile at profile page
+ */
+function cancelEditProfile(){
+    $("#profile-info").removeClass("hidden");
+    $("#profile-form").addClass("hidden");
+}
+
