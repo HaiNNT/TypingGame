f47ba95b3c371969461845eb6f5cc6fca73ef72f
Nguyễn Ngọc Thanh Hải <HaiNNT@users.noreply.github.com>
2014-10-21 15:04:12 +0700

    change package name

--- Code/Backend/TypingGame/src/java/services/AdminService.java
+++ Code/Backend/TypingGame/src/java/services/AdminService.java
@@ -4,12 +4,12 @@
  * and open the template in the editor.
  */
 
-package service;
+package services;
 
 /**
  *
  * @author Administrator
  */
-public class ProfileService {
+public class AdminService {
     
 }
--- Code/Backend/TypingGame/src/java/services/ProfileService.java
+++ Code/Backend/TypingGame/src/java/services/ProfileService.java
@@ -4,12 +4,12 @@
  * and open the template in the editor.
  */
 
-package service;
+package services;
 
 /**
  *
  * @author Administrator
  */
-public class AdminService {
+public class ProfileService {
     
 }
